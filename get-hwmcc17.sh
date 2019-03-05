1f5d298b5776b1ec11fe32ded10a689e50d01184
Makai Mann <makaim@stanford.edu>
2019-03-05 10:30:43 -0800

    Add convenience script for pulling hwmcc benchmarks

---
+++ get-hwmcc17.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir hwmcc17
+cd hwmcc17
+wget http://fmv.jku.at/hwmcc17/hwmcc17-single-benchmarks.tar.xz
+tar xf hwmcc17-single-benchmarks.tar.xz
+rm hwmcc17-single-benchmarks.tar.xz
+cd ../
