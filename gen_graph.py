2b8fa721567c2408b2872eb5fd2ee23f6d40f61d
Makai Mann <makaim@stanford.edu>
2019-03-05 16:04:02 -0800

    Generate graph with BFS from the property, and don't use map solver anymore

--- gen_graph.py
+++ gen_graph.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import argparse
 from cnf_utils import read_cnf, assert_clauses, check_inductiveness, identify_invariants, Clause
+from collections import deque
 from graphviz import Digraph
 from itertools import chain
 from marco import SubsetSolver, MapSolver, enumerate_sets
 import sys
 from z3 import Solver, Not, And
 
-def get_mus(csolver, map_):
+def get_mus(csolver):
     '''
     Returns a single MUS
     '''
-    seed = map_.next_seed()
+    seed = set(range(csolver.n))
     MUS = csolver.shrink(seed)
     return csolver.to_c_lits(MUS)
 
@@ -20,10 +21,9 @@ def get_deps(constraints, npinv):
     constraints.append(npinv)
     csolver = SubsetSolver(constraints)
     l2c = csolver.get_lit_constraint_map()
-    msolver = MapSolver(n=csolver.n)
 
     deps = set()
-    for l in get_mus(csolver, msolver):
+    for l in get_mus(csolver):
         c = l2c[l]
         if c.get_id() != npinv.get_id():
             deps.add(Clause(c))
@@ -39,15 +39,17 @@ def check_single_inv_induction(solver, inv, npinv):
     solver.pop()
     return res
 
-def debug_printing(pinv2inv, mucs, trans):
+def debug_printing(pinv2inv, trans, prop, include_mapping=True):
     print('+++++++++++++++++++++++ debug printing +++++++++++++++++++++++++++')
     print('trans id =', trans._id)
+    print('prop id =', prop._id)
     print('inv --> primed inv')
-    for pinv, inv in pinv2inv.items():
-        print("{} --> {}".format(inv._id, pinv._id))
-    print('----------------mucs------------------')
-    for oinv, deps in mucs:
-        print("{}: {}".format(oinv._id, [d._id for d in deps]))
+    if include_mapping:
+        for pinv, inv in pinv2inv.items():
+            print("{} --> {}".format(inv._id, pinv._id))
+    # print('----------------mucs------------------')
+    # for oinv, deps in mucs:
+    #     print("{}: {}".format(oinv._id, [d._id for d in deps]))
     print('+++++++++++++++++++++ end debug printing +++++++++++++++++++++++++')
 
 def main():
@@ -72,27 +74,30 @@ def main():
     inv_primed_cand = read_cnf(args.invprime_cand_filename)
     outname = args.outname
 
-    pinv2inv = identify_invariants(trans, inv_cand, inv_primed_cand)
+    # the first invariant is assumed to be the property
+    # we don't want to remove that, so take it out now and add it back afterwards
+    prop = inv_cand[0]
+    primeprop = inv_primed_cand[0]
+    pinv2inv = identify_invariants(trans, inv_cand[1:], inv_primed_cand[1:])
+    pinv2inv[primeprop] = prop
 
-    if len(pinv2inv) == 0:
-        print("Zero invariants, aborting")
+    if len(pinv2inv) == 1:
+        print("Zero invariants (other than property), aborting")
         sys.exit(0)
 
     invs = pinv2inv.values()
     pinvs = pinv2inv.keys()
-    s = Solver()
-    assert_clauses(s, trans)
-    print('Check inductiveness of invariants')
-    assert check_inductiveness(s, pinv2inv)
 
-    print("Running MARCO...")
+    # look up the other way
+    inv2pinv = {}
+    for k, v in pinv2inv.items():
+        inv2pinv[v] = k
+
+    print("Finding dependencies...")
 
     z3trans = And([c._expr for c in trans])
-
     clause_trans = Clause(z3trans)
-
     neg_prime_inv_map = {}
-
     constraints = [z3trans]
     for inv in invs:
         constraints.append(inv._expr)
@@ -100,48 +105,64 @@ def main():
     ind_solver = Solver()
     ind_solver.add(z3trans)
 
-    mucs = []
-    count = 0
-    for pi in pinvs:
-        if count % 10 == 0:
-            print('#', end='')
-            sys.stdout.flush()
-        count += 1
-        # need to be able to look up from negated primed invariants
-        npinv = Not(pi._expr)
-        orig_inv = pinv2inv[pi]
-        # check if it's inductive by itself
-        if check_single_inv_induction(ind_solver, orig_inv._expr, npinv):
-            continue
-        mucs.append((orig_inv, get_deps(constraints, npinv)))
+#    debug_printing(pinv2inv, clause_trans, prop, include_mapping=False)
 
-#    debug_printing(pinv2inv, mucs, clause_trans)
+    deps = {}
+    to_visit = deque([prop])
+    visited = set()
+    count = 0
+    while to_visit:
+        if count % 20 == 0:
+            print('#', end='')
+
+        inv = to_visit.pop()
+        pinv = inv2pinv[inv]
+
+#        print('visiting', inv._id, pinv._id)
+
+        if inv in visited:
+#            print('skipping because already in visited')
+            continue
+        else:
+            visited.add(inv)
+            invdeps = get_deps(constraints, Not(pinv._expr))
+#            print("invdeps", [i._id for i in invdeps])
+            for i in invdeps:
+                if i != clause_trans:
+                    to_visit.appendleft(i)
+            deps[inv] = invdeps
+            count += 1
 
     print('\nBuilding graph...')
     dot = Digraph(comment="Induction Graph")
 
-    if len(invs) < 100:
-        for inv in invs:
-            dot.node(str(inv._id))
-        for (inv, deps) in mucs:
-            for d in deps:
-                if d != inv and d != clause_trans:
-                    dot.edge(str(inv._id), str(d._id), label=None)
-    else:
-        # for very large invariants, generate nodes lazily
-        nodes = set()
-        for (inv, deps) in mucs:
-            if len(deps) == 0:
+    to_visit = deque()
+    visited = set()
+    # handle prop specially
+    visited.add(prop)
+    for d in deps[prop]:
+        if d == prop:
+            dot.edge('Prop', 'Prop')
+        elif d != clause_trans:
+            to_visit.appendleft(d)
+            dot.edge('Prop', str(d._id))
+
+    while to_visit:
+        inv = to_visit.pop()
+        if inv in visited:
+            continue
+        visited.add(inv)
+        if inv not in deps:
+            # inv is a leaf
+            continue
+        for d in deps[inv]:
+            if d == prop:
+                dot.edge(str(inv._id), 'Prop')
+            elif d == clause_trans:
                 continue
-            if inv._id not in nodes:
-                nodes.add(inv._id)
-                dot.node(str(inv._id))
-            for d in deps:
-                if d != inv and d != clause_trans:
-                    if d._id not in nodes:
-                        nodes.add(d._id)
-                        dot.node(str(d._id))
-                    dot.edge(str(inv._id), str(d._id), label=None)
+            else:
+                dot.edge(str(inv._id), str(d._id))
+                to_visit.appendleft(d)
 
     dot.render('./%s.dot'%outname)
 
