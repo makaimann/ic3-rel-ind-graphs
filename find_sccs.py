d4ae456f08063930e1f73843e5830454a5cd4099
Makai Mann <makaim@stanford.edu>
2019-03-07 09:23:46 -0800

    Add processing options to find_sccs.py

--- find_sccs.py
+++ find_sccs.py
@@ -17,14 +17,14 @@ class Graph:
         self.graph[u].append(v)
 
     # A function used by DFS
-    def DFSUtil(self,v,visited, rev):
+    def DFSUtil(self,v,visited):
         # Mark the current node as visited and print it
         visited[v]= True
-        print(rev[v], end=" ")
+        yield v
         #Recur for all the vertices adjacent to this vertex
         for i in self.graph[v]:
             if visited[i]==False:
-                self.DFSUtil(i,visited, rev)
+                yield from self.DFSUtil(i,visited)
 
 
     def fillOrder(self,v,visited, stack):
@@ -51,7 +51,7 @@ class Graph:
 
     # The main function that finds and prints all strongly
     # connected components
-    def printSCCs(self, rev):
+    def getSCCs(self):
 
         stack = []
         # Mark all the vertices as not visited (For first DFS)
@@ -69,11 +69,12 @@ class Graph:
         visited =[False]*(self.V)
 
         # Now process all vertices in order defined by Stack
+        sccs = []
         while stack:
             i = stack.pop()
             if visited[i]==False:
-                gr.DFSUtil(i, visited, rev)
-                print()
+                sccs.append(list(gr.DFSUtil(i, visited)))
+        return sccs
 
 if __name__ == "__main__":
     import argparse
@@ -82,9 +83,14 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Find Strongly Connected Components")
     parser.add_argument('input_file', help='Pickled list of edges')
+    parser.add_argument('--proc', metavar="<PROC_TYPE>", help='The type of processing to do: <list|num|hist>', default='list')
     args = parser.parse_args()
 
     edges = pickle.load(open(args.input_file, 'rb'))
+
+    proc = args.proc
+    if proc not in {'list', 'num', 'hist'}:
+        raise NotImplementedError("{} processing not implemented".format(proc))
 
     d = dict()
     count = 0
@@ -102,9 +108,35 @@ if __name__ == "__main__":
     for n1, n2 in edges:
         g.addEdge(d[n1], d[n2])
 
-    print ("Following are strongly connected components " +
-           "in given graph")
-
     rev = {v:k for k, v in d.items()}
-    g.printSCCs(rev)
+    sccs = g.getSCCs()
+
+    if proc == "list":
+        print ("Following are strongly connected components " +
+               "in given graph")
+        for scc in sccs:
+            for n in scc:
+                print(rev[n], end=' ')
+            print()
+    elif proc == "num":
+        print('Found {} SCCs of the following lengths:'.format(len(sccs)))
+        lengths = sorted([len(scc) for scc in sccs])
+        print("  " + ", ".join([str(l) for l in lengths]))
+    elif proc == "hist":
+        from matplotlib import pyplot as plt
+
+        hist = defaultdict(int)
+        for scc in sccs:
+            hist[len(scc)] += 1
+
+        length, freq = zip(*hist.items())
+        length = list(map(int, length))
+        freq = list(map(int, freq))
+        plt.bar(length, freq, align='center')
+        plt.xticks(sorted(length))
+        plt.xlabel('Size of SCC')
+        plt.ylabel('Number of SCCs')
+        plt.title('Frequence of SCC sizes')
+        plt.show()
+
     #This code is contributed by Neelam Yadav
