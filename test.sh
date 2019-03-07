d192636da6da46de96b42dc6fe384cb5dc09c868
Makai Mann <makaim@stanford.edu>
2019-03-07 09:42:16 -0800

    Dump pickled list of edges in tests

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TO=120
+TO=3600
 
 for aig in `ls ./hwmcc17/*`; do
     rm -f inv.cnf trans.cnf
@@ -12,7 +12,7 @@ for aig in `ls ./hwmcc17/*`; do
         echo "timed out"
     elif [ "$res" -eq 0 ]; then
         echo "Property was proven within the timeout, generating graph..."
-        ./gen_graph.py -t trans.cnf -i inv.cnf -ip inv-primed.cnf -o "$filename"
+        ./gen_graph.py -t trans.cnf -i inv.cnf -ip inv-primed.cnf -o "$filename" --pickle
     elif [ "$res" -eq 1 ]; then
         echo "Property doesn't hold -- aborting"
     else
