5af2fb76c9a1bdbf8ca5ac12ad1080b40fea7b7b
Makai Mann <makaim@stanford.edu>
2019-04-03 08:48:24 -0700

    Replace recursive DFS with iterative implementation for finding SCCs

--- find_sccs.py
+++ find_sccs.py
@@ -10,77 +10,97 @@ from collections import defaultdict
 import pathlib
 import pickle
 import sys
+from typing import List, Optional, Set
 
-#This class represents a directed graph using adjacency list representation
 class Graph:
 
-    def __init__(self,vertices):
-        self.V= vertices #No. of vertices
-        self.graph = defaultdict(list) # default dictionary to store graph
+    def __init__(self, nodes:List[int])->None:
+        self.nodes = nodes
+        self.edges = defaultdict(list)
 
-    # function to add an edge to graph
-    def addEdge(self,u,v):
-        self.graph[u].append(v)
+    def addEdge(self, u, v):
+        self.edges[u].append(v)
 
-    # A function used by DFS
-    def DFSUtil(self,v,visited):
-        # Mark the current node as visited and print it
-        visited[v]= True
-        yield v
-        #Recur for all the vertices adjacent to this vertex
-        for i in self.graph[v]:
-            if visited[i]==False:
-                yield from self.DFSUtil(i,visited)
+    def transpose(self):
+        gt = Graph(self.nodes)
 
+        # reverse all the edges
+        for s, dns in self.edges.items():
+            for dn in dns:
+                gt.addEdge(dn, s)
+        return gt
 
-    def fillOrder(self,v,visited, stack):
-        # Mark the current node as visited
-        visited[v]= True
-        #Recur for all the vertices adjacent to this vertex
-        for i in self.graph[v]:
-            if visited[i]==False:
-                self.fillOrder(i, visited, stack)
-        stack = stack.append(v)
+def dfs(g:Graph)->List[int]:
 
+    # which have been added to the finish_stack
+    # cheaper to query a set for membership, so duplicating storage
+    processed = set()
+    finish_stack = []
 
-    # Function that returns reverse (or transpose) of this graph
-    def getTranspose(self):
-        g = Graph(self.V)
+    visited = defaultdict(bool)
+    for start in g.nodes:
+        if visited[start]:
+            continue
+        dfs_stack = [start]
+        while dfs_stack:
+            n = dfs_stack.pop()
+            if not visited[n]:
+                dfs_stack.append(n)
+                visited[n] = True
+                for dn in g.edges[n]:
+                    if not visited[dn]:
+                        assert dn not in processed
+                        dfs_stack.append(dn)
+            else:
+                if n not in processed:
+                    assert sum(1 for x in g.edges[n] if not visited[x]) == 0, \
+                        "Expecting all DFS paths from this node to have already been explored"
+                    processed.add(n)
+                    # make sure it's not in the dfs_stack
+                    if n not in finish_stack:
+                        # finished DFS at this node
+                        finish_stack.append(n)
 
-        # Recur for all the vertices adjacent to this vertex
-        for i in self.graph:
-            for j in self.graph[i]:
-                g.addEdge(j,i)
-        return g
+    assert len(finish_stack) == len(g.nodes)
+    return finish_stack
 
+def get_sccs(g:Graph)->List[Set[int]]:
+    sccs = []
 
+    finish_stack = dfs(g)
+    gt = g.transpose()
 
-    # The main function that finds and prints all strongly
-    # connected components
-    def getSCCs(self):
+    # reverse dfs to get SCCs
+    visited = defaultdict(bool)
+    while finish_stack:
+        start = finish_stack.pop()
+        if visited[start]:
+            continue
+        scc = set()
+        dfs_stack = [start]
+        while dfs_stack:
+            n = dfs_stack.pop()
+            if not visited[n]:
+                dfs_stack.append(n)
+                scc.add(n)
+                visited[n] = True
+                for dn in gt.edges[n]:
+                    if not visited[dn]:
+                        dfs_stack.append(dn)
+        sccs.append(scc)
+    return sccs
 
-        stack = []
-        # Mark all the vertices as not visited (For first DFS)
-        visited =[False]*(self.V)
-        # Fill vertices in stack according to their finishing
-        # times
-        for i in range(self.V):
-            if visited[i]==False:
-                self.fillOrder(i, visited, stack)
+# Simple Test
+# if __name__ == "__main__":
+#     g = Graph([0, 1, 2, 3, 4])
+#     g.addEdge(1, 0)
+#     g.addEdge(0, 2)
+#     g.addEdge(2, 1)
+#     g.addEdge(0, 3)
+#     g.addEdge(3, 4)
 
-        # Create a reversed graph
-        gr = self.getTranspose()
-
-        # Mark all the vertices as not visited (For second DFS)
-        visited =[False]*(self.V)
-
-        # Now process all vertices in order defined by Stack
-        sccs = []
-        while stack:
-            i = stack.pop()
-            if visited[i]==False:
-                sccs.append(list(gr.DFSUtil(i, visited)))
-        return sccs
+#     sccs = get_sccs(g)
+#     print(sccs)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find Strongly Connected Components")
@@ -133,13 +153,13 @@ if __name__ == "__main__":
             count += 1
 
     # Create a graph given in the above diagram
-    g = Graph(count)
+    g = Graph(list(d.values()))
 
     for n1, n2 in edges:
         g.addEdge(d[n1], d[n2])
 
     rev = {v:k for k, v in d.items()}
-    sccs = g.getSCCs()
+    sccs = get_sccs(g)
 
     if proc == "list":
         print ("Following are strongly connected components " +
