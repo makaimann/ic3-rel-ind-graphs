0e21fc6c3455cf38af9ccaf074d787bc34bccfa7
Makai Mann <makaim@stanford.edu>
2019-04-12 12:53:52 -0700

    Add bfs option

--- find_sccs.py
+++ find_sccs.py
@@ -6,11 +6,13 @@
 #     original code has been modified here
 
 import argparse
-from collections import defaultdict
+from collections import defaultdict, deque
 import pathlib
 import pickle
 import sys
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Dict
+
+PROP='Prop'
 
 class Graph:
 
@@ -31,6 +33,10 @@ class Graph:
         return gt
 
 def dfs(g:Graph)->List[int]:
+    '''
+    Returns a "finish stack" where nodes have been added to the stack when
+    every path from that node has already been explored
+    '''
 
     # which have been added to the finish_stack
     # cheaper to query a set for membership, so duplicating storage
@@ -64,8 +70,31 @@ def dfs(g:Graph)->List[int]:
     assert len(finish_stack) == len(g.nodes)
     return finish_stack
 
-def bfs(g:Graph)->List[int]:
-    pass
+def bfs(g:Graph, start:int)->Dict[int, Optional[int]]:
+    '''
+    Takes a graph and starting node and returns a dictionary labeling the distance from the start node
+    for each node in the graph.
+    '''
+    assert start in g.nodes, "Expecting start node to be in nodes"
+
+    labeled_nodes = dict()
+    for n in g.nodes:
+        labeled_nodes[n] = None
+
+    visited = defaultdict(bool)
+    queue = deque([start])
+    labeled_nodes[start] = 0
+    while queue:
+        n = queue.popleft()
+        if not visited[n]:
+            visited[n] = True
+            for dn in g.edges[n]:
+                queue.append(dn)
+                if labeled_nodes[dn] is None: # first time we've encountered this node so far
+                    assert labeled_nodes[n] is not None, "Expecting previous node to have a distance"
+                    labeled_nodes[dn] = labeled_nodes[n] + 1
+
+    return labeled_nodes
 
 def get_sccs(g:Graph)->List[Set[int]]:
     sccs = []
@@ -108,7 +137,7 @@ def get_sccs(g:Graph)->List[Set[int]]:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find Strongly Connected Components")
     parser.add_argument('input_file', help='Pickled list of edges (.pkl), or string of hyperedges that can be evaluated (.out)')
-    parser.add_argument('--proc', metavar="<PROC_TYPE>", choices=['list', 'num', 'hist'],
+    parser.add_argument('--proc', metavar="<PROC_TYPE>", choices=['list', 'num', 'hist', 'bfs'],
                         help='The type of processing to do: <list|num|hist>', default='num')
     parser.add_argument('--remove', metavar="<NODES_TO_REMOVE>", help='A semicolon delimited list of node names to remove', default='')
     args = parser.parse_args()
@@ -144,9 +173,6 @@ if __name__ == "__main__":
                     edges.append((src_node, dn))
                     nodes.add(dn)
 
-    if proc not in {'list', 'num', 'hist'}:
-        raise NotImplementedError("{} processing not implemented".format(proc))
-
     if remove:
         print('Removing', remove)
         for r in remove:
@@ -157,11 +183,11 @@ if __name__ == "__main__":
         print("Removed {} edges".format(edge_len - len(edges)))
 
     to_graph_node = dict()
-    count = 0
+    dist = 0
     for node in nodes:
         if node not in to_graph_node:
-            to_graph_node[node] = count
-            count += 1
+            to_graph_node[node] = dist
+            dist += 1
 
     # Create a graph given in the above diagram
     g = Graph(list(to_graph_node.values()))
@@ -170,9 +196,9 @@ if __name__ == "__main__":
         g.addEdge(to_graph_node[n1], to_graph_node[n2])
 
     from_graph_node = {v:k for k, v in to_graph_node.items()}
-    sccs = get_sccs(g)
 
     if proc == "list":
+        sccs = get_sccs(g)
         print ("Following are strongly connected components " +
                "in given graph")
         for scc in sccs:
@@ -180,12 +206,14 @@ if __name__ == "__main__":
                 print(from_graph_node[n], end=' ')
             print()
     elif proc == "num":
+        sccs = get_sccs(g)
         print('Found {} SCCs of the following lengths:'.format(len(sccs)))
         hist = defaultdict(int)
         for scc in sccs:
             hist[len(scc)] += 1
         print(hist)
     elif proc == "hist":
+        sccs = get_sccs(g)
         from matplotlib import pyplot as plt
 
         hist = defaultdict(int)
@@ -202,3 +230,15 @@ if __name__ == "__main__":
         plt.ylabel('Number of SCCs')
         plt.title('Occurrences of SCC sizes')
         plt.show()
+    elif proc == 'bfs':
+        labeled_nodes = bfs(g, to_graph_node[PROP])
+        max_distance = max(filter(lambda x: x is not None, labeled_nodes.values()))
+        bfs_count = [0]*(max_distance+1)
+
+        for n, dist in labeled_nodes.items():
+            if dist is None:
+                continue
+            else:
+                bfs_count[dist] += 1
+
+        print(bfs_count)
